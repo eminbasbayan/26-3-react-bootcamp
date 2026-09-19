49dbfd9b1b309e8873de26e31fe9e6716dd03eb7
eminbasbayan <emin.basbayan@icloud.com>
2026-09-19 21:52:42 +0300

    props mantığı

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,19 @@
 import './ProductCard.css';
 
-function ProductCard() {
+function ProductCard(props) {
+  const image = 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png';
+  const title = 'Product Title';
+  const price = 200;
+
+  console.log(props.fullName);
+  
+
   return (
     <div className="product-card">
-      <img
-        className="product-image"
-        src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
-        alt="Çanta Görseli"
-      />
+      <img className="product-image" src={image} alt="Çanta Görseli" />
       <div className="product-info">
-        <strong className="product-title">Product Title</strong>
-        <span className="product-price">100₺</span>
+        <strong className="product-title">{title}</strong>
+        <span className="product-price">{price}₺</span>
       </div>
     </div>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,15 @@
 import ProductCard from './ProductCard';
-import "./Products.css";
+import { productsData } from '../data/productsData';
+import './Products.css';
 
 const Products = () => {
   return (
     <div className="products">
       <h1>Products Component</h1>
       <div className="products-wrapper">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {productsData.map((product) => (
+          <ProductCard fullName="Emin Başbayan" />
+        ))}
       </div>
     </div>
   );
