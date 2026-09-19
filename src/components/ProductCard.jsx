2575c9d18287eacaed24586909287ff7881b7beb
eminbasbayan <emin.basbayan@icloud.com>
2026-09-19 22:00:54 +0300

    description eklendi

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,14 @@
 import './ProductCard.css';
 
 function ProductCard({ product }) {
-  const { image, title, price } = product;
+  const { image, title, price, description } = product;
 
   return (
     <div className="product-card">
       <img className="product-image" src={image} alt="Çanta Görseli" />
       <div className="product-info">
         <strong className="product-title">{title}</strong>
+        <p className='product-description'>{description}</p>
         <span className="product-price">{price}₺</span>
       </div>
     </div>
