ad3b34570922eb392de30250335f899ef3fa1aac
eminbasbayan <emin.basbayan@icloud.com>
2026-09-24 21:20:08 +0300

    useState hook kullanımı

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Counter from './components/Counter';
 import Products from './components/Products';
 
 function App() {
   return (
     <div className="app">
       <h1>App Component</h1>
+      <Counter />
       <Products />
     </div>
   );
---
+++ src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  function arttir() {
+    // count = count + 1 hatalı kullanım
+    // setCount(count + 1);
+    setCount((prevState) => prevState + 1);
+  }
+
+  function azalt() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="counter">
+      <button onClick={arttir}>Arttır</button>
+      <span>{count}</span>
+      <button onClick={azalt}>Azalt</button>
+    </div>
+  );
+};
+
+export default Counter;
