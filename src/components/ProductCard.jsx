7af7385beaac99aa5caf9aa8a1cd1a5477f92dd1
eminbasbayan <emin.basbayan@icloud.com>
2026-09-24 21:32:53 +0300

    props children

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react';
+import Button from './Button';
 import './ProductCard.css';
 
 function ProductCard({ product }) {
   const { image, title, price, description } = product;
-  const [titleState, setTitleState] = useState(title)
 
-  function titleChange(){
-    setTitleState("Title Değişti!")
-  }
- 
   return (
     <div className="product-card">
       <img className="product-image" src={image} alt="Çanta Görseli" />
       <div className="product-info">
-        <strong className="product-title">{titleState}</strong>
-        <p className='product-description'>{description}</p>
-        <button onClick={titleChange}>Title Değiştir</button>
+        <strong className="product-title">{title}</strong>
+        <p className="product-description">{description}</p>
         <span className="product-price">{price}₺</span>
+        <div className="product-buttons">
+          <Button>
+            <strong>Sepete Ekle</strong>
+          </Button>
+        </div>
       </div>
     </div>
   );
